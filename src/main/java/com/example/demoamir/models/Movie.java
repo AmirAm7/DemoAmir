a539d9b8b740064cb34b2b31d81c9859d0a0cc33
Amir <ah.amiri.1985@gmail.com>
2022-06-02 14:02:02 +0200

    create Movie-Model

--- src/main/java/com/example/demoamir/models/Movie.java
+++ src/main/java/com/example/demoamir/models/Movie.java
@@ -1,4 +1,44 @@
 package com.example.demoamir.models;
 
+import java.lang.annotation.Repeatable;
+import java.time.LocalDate;
+
+
 public class Movie {
+    private String title;
+    private int id;
+    private String description;
+    private LocalDate years;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getYears() {
+        return years;
+    }
+
+    public void setYears(LocalDate years) {
+        this.years = years;
+    }
 }
