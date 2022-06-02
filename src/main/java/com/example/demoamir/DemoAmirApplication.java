759ef32b257aca6916dad971a5f47935c5028001
Amir <ah.amiri.1985@gmail.com>
2022-06-02 13:17:49 +0200

    create project

---
+++ src/main/java/com/example/demoamir/DemoAmirApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoamir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAmirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAmirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoamir/DemoAmirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoamir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAmirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
